0d71d257b949ab9ac6c5644c708feb5a4ea5ca31
Adil Reza <adilreza043@gmail.com>
2020-10-19 18:01:50 +0600

    initilizing the context api

---
+++ samplecontext/src/contextfile/moviecontext.js
@@ -0,0 +1,29 @@
+import React, {useState, createContext}  from 'react'
+
+export const MovieContext = createContext();
+
+export const MovieProvider = (props)=>{
+    const [movies, setMovies] = useState([
+        {
+            name: 'Harry Potter',
+            price: '$10',
+            id: 23440
+        },
+        {
+            name: 'Gladiator',
+            price: '$15',
+            id: 998440
+        },
+        {
+            name: 'Avenger End Game',
+            price: '$30',
+            id: 24540
+        }
+    ]);
+    return (
+        <MovieContext.Provider value={"Hello adil"}>
+            {props.children}
+        </MovieContext.Provider>
+
+    )
+}
